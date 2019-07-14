682edcdb04a5c95bd22e33cd3cab4d7d5e4194a8
Lenovo <rybynok@mail.ru>
2019-07-14 22:56:21 +0300

    finish

--- my-test.py
+++ my-test.py
@@ -5,13 +5,17 @@ def main(arguments):
     if len(arguments) == 5 and arguments[-1] == 'b':
 
         if arguments[2] == '-eq':
-            return int(arguments[1]) == int(arguments[3])
+            if int(arguments[1]) == int(arguments[3]):
+                return True
         elif arguments[2] == '-lt':
-            return int(arguments[1]) < int(arguments[3])
+            if int(arguments[1]) < int(arguments[3]):
+                return True
         elif arguments[2] == '-gt':
-            return int(arguments[1]) > int(arguments[3])
+            if int(arguments[1]) > int(arguments[3]):
+                return True
         elif arguments[2] == '-ne':
-            return int(arguments[1]) != int(arguments[3])
+            if int(arguments[1]) != int(arguments[3]):
+                return True
     else:
         return False
 
